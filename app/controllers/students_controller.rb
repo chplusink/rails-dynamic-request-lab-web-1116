b5d4f1e417b328c716fac1821f2c705f355774ce
Vic Lam <lam.vic.k@gmail.com>
2016-12-22 16:10:13 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,9 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    # @student = Student.find(:id)
+    @student = Student.all.first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'students', to: 'students#index'
+
+  get 'students/:id', to: 'students#show'
+
   resources :students, only: :index
 end
